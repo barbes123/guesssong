dc6d3d740b6ca71f0df6dd6c0b198a104aef4397
B420 <B420@B420>
2026-01-20 19:27:47 +0200

    stable

--- data/round1_index.ts
+++ data/round1_index.ts
@@ -1,7 +1,5 @@
 import { defaultSet } from './round1_default';
 import { round1v2Set } from './round1_v2';
-// Import other sets when you create them
-// import { newyearSpecial } from './round1_special';
 
 export const round1Sets = {
   default: defaultSet,
